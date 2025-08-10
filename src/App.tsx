9ed911d540248cbee482217d934d7b01afdc4abe
Jørund Jenssen Berg <jjberg@gmail.com>
2025-08-10 18:29:46 +0200

    Prøvde å lage egenlagd komponent med flere HTML tagger i (parentes rundt så går det)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import Message from './Message';
+import JJBergParagraph from './JJBergParagraph';
 
 function App() {
   // return <div><Message></Message></div> // Opening og closing bracket
-  return <div><Message/></div> // Kortere syntaks (opening og closing i en og samme tag)
+  return <div><Message/><JJBergParagraph/></div> // Kortere syntaks (opening og closing i en og samme tag)
 }
 
 export default App;
