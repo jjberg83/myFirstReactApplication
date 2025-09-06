3a9494c15dc126ebd79e85963352abac805b750c
Jørund Jenssen Berg <jjberg@gmail.com>
2025-09-06 09:51:58 +0200

    Startet på passing children leksjon

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,7 @@
-import ListGroup_9 from "./components/ListGroup_9";
+import Alert_1 from "./components/Alert_1";
 
 function App() {
-  // Denne blir endret på inni selve komponenten, og det bør man absolutt ikke gjøre!
-  let klubber = [
-    "Viking",
-    "Bodø Glimt",
-    "Tromsø",
-    "Brann",
-    "Molde",
-    "Manchester United",
-  ];
-  let overskrift = "ListGroup_9";
-
-  const handleSelectItem = (element: string) => console.log(element);
-
-  return (
-    <>
-      <ListGroup_9
-        elements={klubber}
-        heading={overskrift}
-        onSelectItem={handleSelectItem}
-      />
-    </>
-  );
+  return <Alert_1 />;
 }
 
 export default App;
---
+++ src/components/Alert_1.tsx
@@ -0,0 +1,11 @@
+// Building components > Passing children
+
+const Alert_1 = () => {
+  return (
+      <div className="alert alert-warning ">
+          Alert beskjed her!
+      </div>
+  )
+}
+
+export default Alert_1;
