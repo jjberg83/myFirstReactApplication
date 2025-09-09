2638e3e925a8cd7c548693d06744be6a0e7a6ddf
Jørund Jenssen Berg <jjberg@gmail.com>
2025-09-09 06:29:41 +0200

    Challenge 1 på ny

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import Alert_1 from "./components/Alert_1";
+import BootstrapButton from "./components/BoostrapButton";
 
 function App() {
-  const beskjed = "Sjokolade er det beste snopet!";
+  const handleOnSelectedItem = () => console.log("Knapp er trykket på");
   return (
     <>
-      {/* Under gir vi teksten som skal være inni Alert som et vanlig argument (eller prop) */}
-      {/* <Alert_1 beskjed={beskjed} /> */}
-
-      {/* Og her bruker vi fortsatt prop. Men ved å kalle propen "children" i interfacet,
-      kan man legge den inn som en child mellom åpne og lukke tagen til Alert_1.*/}
-      {/* <Alert_1>{beskjed}</Alert_1> */}
-
-      {/* Akkurat det samme som den over, men her legger vil til HTML kode, må da definere children som type ReactNode i interfacet */}
-      <Alert_1>
-        Sjokolade er det <strong>beste</strong> snopet!
-      </Alert_1>
+      {/* Legg merke til at vi ikke gir noen farge her (men kan hvis vi vil) */}
+      <BootstrapButton onSelectedItem={handleOnSelectedItem}>
+        Bouvet ASA
+      </BootstrapButton>
     </>
   );
 }
---
+++ src/components/BoostrapButton.tsx
@@ -0,0 +1,53 @@
+import React, { ReactNode } from "react";
+
+// Dette er challenge 1
+
+interface Props {
+  onSelectedItem: () => void;
+  color?:
+    | "primary"
+    | "secondary"
+    | "success"
+    | "danger"
+    | "warning"
+    | "info"
+    | "light"
+    | "dark"
+    | "link"; // Legg merke til at color er optional. Den har en default verdi i Props, som er fallback hvis den ikke blir definert i app.
+  children: ReactNode;
+}
+
+const BoostrapButton = ({
+  onSelectedItem,
+  color = "warning",
+  children,
+}: Props) => {
+  return (
+    <button
+      type="button"
+      className={`btn btn-${color}`}
+      onClick={onSelectedItem}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default BoostrapButton;
+
+// Kode tilhørende App.tsx
+// import BootstrapButton from "./components/BoostrapButton";
+
+// function App() {
+//   const handleOnSelectedItem = () => console.log("Knapp er trykket på");
+//   return (
+//     <>
+//       {/* Legg merke til at vi ikke gir noen farge her (men kan hvis vi vil) */}
+//       <BootstrapButton onSelectedItem={handleOnSelectedItem}>
+//         Bouvet ASA
+//       </BootstrapButton>
+//     </>
+//   );
+// }
+
+// export default App;
