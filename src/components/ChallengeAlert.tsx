c574ba3d5d6246641c4a88bb1d4352713d6aa365
Jørund Jenssen Berg <jjberg@gmail.com>
2025-09-10 21:43:58 +0200

    La inn app.tsx koden nederst

--- src/components/ChallengeAlert.tsx
+++ src/components/ChallengeAlert.tsx
@@ -58,3 +58,17 @@ const ChallengeAlert = ({
 export default ChallengeAlert;
 
 // Kode tilhørende App.tsx
+// import ChallengeAlert from "./components/ChallengeAlert";
+
+// function App() {
+//   const handleOnSelectedItem = () => console.log("Knapp er trykket på");
+//   return (
+//     <>
+//       <ChallengeAlert onSelectedItem={handleOnSelectedItem}>
+//         Vis meg alert
+//       </ChallengeAlert>
+//     </>
+//   );
+// }
+
+// export default App;
