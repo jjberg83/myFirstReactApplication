9b92da91d1a19ce9079d35578b4180b700c220a1
Jørund Jenssen Berg <jjberg@gmail.com>
2025-09-10 21:11:47 +0200

    Made two component, where I think I only need one

---
+++ src/components/ChallengeAlert.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+// Dette er challenge 2
+
+const ChallengeAlert = () => {
+  return (
+    <div
+      className="alert alert-warning alert-dismissible fade show"
+      role="alert"
+    >
+      <strong>Holy guacamole!</strong> You should check in on some of those
+      fields below.
+      <button
+        type="button"
+        className="close"
+        data-dismiss="alert"
+        aria-label="Close"
+      >
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  );
+};
+
+export default ChallengeAlert;
